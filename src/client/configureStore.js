d61ab6dd1c597523e3400d80035d70b4f1402d54
molly2molly <489573239@qq.com>
2018-08-31 13:52:46 +0800

    update

--- src/client/configureStore.js
+++ src/client/configureStore.js
@@ -7,10 +7,17 @@ import rootReducer from "./reducers";
 const loggerMilddleware = createLogger();
 
 export default function configureStore(preloadedState) {
-  const middlewares = [thunkMiddleware, loggerMilddleware];
+  const middlewares = [];
+  middlewares.push(thunkMiddleware);
+  if (process.env.NODE_ENV === "development") {
+    middlewares.push(loggerMilddleware);
+  }
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
-  const enhancers = [middlewareEnhancer, monitorReducerEnhancer];
+  const enhancers = [middlewareEnhancer];
+  if (process.env.NODE_ENV === "development") {
+    enhancers.push(monitorReducerEnhancer);
+  }
   const composedEnhancers = compose(...enhancers);
 
   const store = createStore(rootReducer, preloadedState, composedEnhancers);
